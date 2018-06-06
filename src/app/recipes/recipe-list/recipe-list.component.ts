c7cecfea63be055fef6b427744a33b61d7bc5f78
Gohar Robert <grobert14@georgefox.edu>
2018-06-06 14:25:09 -0700

    project beginnings

---
+++ src/app/recipes/recipe-list/recipe-list.component.ts
@@ -0,0 +1,19 @@
+import { Component, OnInit } from '@angular/core';
+import { Recipe } from '../recipe.model';
+
+@Component({
+  selector: 'app-recipe-list',
+  templateUrl: './recipe-list.component.html',
+  styleUrls: ['./recipe-list.component.css']
+})
+export class RecipeListComponent implements OnInit {
+  recipes: Recipe[] = [
+      new Recipe('Apple Pie', 'Pie made with apples', 'https://c1.staticflickr.com/3/2284/2070772294_3d0f1eaa6b_b.jpg'),
+      new Recipe('Apple Pie', 'Pie made with apples', 'https://c1.staticflickr.com/3/2284/2070772294_3d0f1eaa6b_b.jpg')
+  ];
+  constructor() { }
+
+  ngOnInit() {
+  }
+
+}
